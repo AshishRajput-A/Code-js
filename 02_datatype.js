e31814a7e6864d401bd0b6f4a279640536a29450
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-02-08 12:09:18 +0000

    add datatype

---
+++ 02_datatype.js
@@ -0,0 +1,22 @@
+const Username = "Ashish Rajput";
+let AccountID = "ashish2004"
+var Password = "00002345"
+
+
+console.table([Username , AccountID,Password]);
+
+
+/* 
+
+const  => can't be reinitilize or recreate
+let  => can be reinitilize but can't be recreate
+var  => can be reinitilize and recreate
+
+
+note => Prefer not use var 
+because of issus in block scope and functional scope
+
+*/
+
+
+
