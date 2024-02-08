ff2362daf64f0faefe293c754f2a918f6c79b1c4
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-02-08 12:12:37 +0000

    add notes

--- 02_datatype.js
+++ 02_datatype.js
@@ -3,7 +3,11 @@ let AccountID = "ashish2004"
 var Password = "00002345"
 
 
+
 console.table([Username , AccountID,Password]);
+
+console.log(typeof(null));       // object
+console.log(typeof(undefined));  // undefine
 
 
 /* 
@@ -18,5 +22,29 @@ because of issus in block scope and functional scope
 
 */
 
+
+/*
+
+In JavaScript, there are primarily two categories of data types: primitive data types and reference data types.
+
+2. Primitive Data Types:
+
+Number: Represents numeric values, e.g., 5, 3.14.
+String: Represents textual data, e.g., "Hello, World!".
+Boolean: Represents true or false.
+Undefined: Represents a variable that has been declared but not assigned a value.
+Null: Represents an intentional absence of any object value.
+Symbol (introduced in ECMAScript 6): Represents a unique and immutable value and is often used as an identifier for object properties.
+
+2. Reference Data Types:
+
+Object: Represents a collection of key-value pairs, e.g., {name: "John", age: 30}.
+Array: Represents a collection of elements, e.g., [1, 2, 3].
+Function: Represents a callable object, e.g., function add(a, b) { return a + b; }.
+JavaScript is a dynamically typed language, meaning you don't need to explicitly declare the data type of a variable. The type of the variable is determined dynamically at runtime based on the value assigned to it.
+
+
+
+*/ 
 
 
