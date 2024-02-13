05e0520820f0a7bb65d0f04b9575d9696bbac506
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-02-13 10:57:37 +0000

    Comparision

--- 04_comparision.js
+++ 04_comparision.js
@@ -8,12 +8,12 @@
 // console.log("02" > 1); //true
 
 // console.log(null > 0); //false
-// console.log(null < 0); //false
+// console.log(null < 0); //false      // avoid this kind of conversion
 // console.log(null >= 0); //true
 // console.log(null <= 0); //true
 
 // console.log( undefined == 0); //false
-// console.log( undefined > 0); //false
+// console.log( undefined > 0); //false    // avoid this kind of conversion
 // console.log( undefined < 0); //false
 
 console.log("2" == 2 );  //true
