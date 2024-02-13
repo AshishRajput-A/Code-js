b103549c08a49c64a8f06e6fef5aa133c171e310
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-02-13 10:54:03 +0000

    type_conversion

---
+++ 03_conversion.js
@@ -0,0 +1,54 @@
+//Number conversion
+/*let a  =undefined;
+console.log(typeof (a));
+
+let valNum = Number(a);
+console.log(valNum);
+console.log(typeof(valNum));*/
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1;  false =>0
+
+
+// boolean conversion
+/*let isLoggedIn = 5;
+let booleanIsLogedIn = Boolean(isLoggedIn);
+console.log(booleanIsLogedIn);*/
+// 1 => true; 0 => false
+// ""=> false "ashish" => true
+
+
+//string conversion
+/*let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);*/
+
+
+/****************************** Operation **************************/
+
+let value = 3;
+let nagValue = -value;
+// console.log(nagValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let  gameCounter =  100;
+++gameCounter;
+gameCounter++;
+++gameCounter;
+++gameCounter;
+++gameCounter;
+gameCounter++;
+
+// console.log(gameCounter);
+
+
+
