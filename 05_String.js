c8f9a66e9bd8f2d109a3dd3f281403ca0a443223
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-02-23 12:47:39 +0000

    String-Methods

---
+++ 05_String.js
@@ -0,0 +1,31 @@
+
+// first how to declare string  => it gives you array of string
+const str1 = new String("Ashish");
+// console.log(str1);
+// console.log(typeof str1);
+
+const name ="AshishRajput";
+const repoCount = 50;
+// console.log(`${name}${repoCount} value`);
+
+// console.log(name.length);  //length
+// console.log(name.toUpperCase());  // toUpperCase()
+// console.log(name.toLowerCase()); //toLowerCase()
+//console.log(name.charAt(3)); // charAt()
+// console.log(name.indexOf('i')); // indexOf()
+//console.log(name.replace("-","")); // replace()
+// console.log(name.substring(0,4)); // substring()
+// console.log(name.slice(-6,3)); //slice()
+
+const name1 =  "        Ashish      "
+// console.log(name1);
+// console.log(name1.trim());     // trim()
+
+const url ="https://ashish.com//ashish%20rajput";
+// console.log(url.replace("%20" , "-"));
+
+// console.log(url.includes("rajput"));   // includes()
+
+
+const game = "Ashish-Rajput-WedDev" // split()
+console.log(game.split("-")); // it split the screen acording condition
