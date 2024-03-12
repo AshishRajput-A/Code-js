c88e032165d396fe5598c0d7fb657bba82235662
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-03-12 04:11:15 +0000

    scope in javascript

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,77 @@
+
+
+let a =300;
+
+if (true) {
+
+    let a =10;
+    const b =20;
+    var c = 30;
+    // console.log("inner.:" , a);
+
+}
+
+
+
+// console.log("a.: ",a);
+// // console.log(b);
+// console.log("c.: ",c);
+
+
+
+
+// function one(){
+
+//     const username1 = "ashish"
+
+
+//     function two(){
+
+//         const username2 = "vijay"
+
+//         console.log("user2.:",username2);
+//     }
+//     two()
+
+    
+//     console.log("user1.:",username1);
+
+// }
+// one()
+
+
+
+if(true)
+{
+    const username = "ashish";
+
+    if(username === "ashish")
+    {
+    const website = "www.youtube.com";
+
+    // console.log(username, "  " + website);
+
+
+    }
+    // console.log("website.:",website);
+}
+// console.log("username.:" , username);
+
+
+function addone(num) {
+
+    return num + 1;
+
+}
+
+
+console.log(addone(5));
+
+const addTwo =  function (num) {
+    return num + 2;
+}
+
+console.log(addTwo(5));
+
+
+
