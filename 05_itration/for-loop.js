3969ad1a965dd98cbfe93d21c1a7a4e98de4f6eb
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-03-16 11:29:43 +0000

    for-loop in js

---
+++ 05_itration/for-loop.js
@@ -0,0 +1,53 @@
+
+
+// for 
+
+for(let i =1; i<=10;i++)
+{
+    const element = i;
+
+    // if(element == 5) 
+    // console.log("5 is best number ");
+    
+
+    // console.log(element);
+
+}
+
+/*
+for(let i =1; i<=1;i++)
+{
+    for(let j=1; j<=10; j++)
+    {
+        console.log(  `${i} * ${j} = ${i*j}` );
+        
+    }
+    
+}
+   */
+  
+let myarr = ["flash" , "batman" , "superman", "spiderman" , "ironman"]
+
+
+// for (let i =0;i < myarr.length; i++)
+// {
+//    // console.log(myarr[i]);
+// }
+
+
+// break  continue
+
+for(let  i =1; i<=15;i++)
+{
+
+    // if(i== 5)
+    // {
+    //     break;
+    // }
+    if(i== 5)
+    {
+        continue;
+    }
+
+    console.log(i);
+}
