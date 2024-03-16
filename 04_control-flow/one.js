ff8d596921d18fa8d9e12139df3a58dc0d72a97e
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-03-16 05:18:04 +0000

    If-else

---
+++ 04_control-flow/one.js
@@ -0,0 +1,67 @@
+// if 
+
+let login = true ;
+
+/*if(login === true)
+console.log("User is Login");
+else
+console.log("User is Logout");*/
+
+
+// < ,  >  , <= , >= , == , != , === , !==
+
+/*if(2 !=="2")                 // its called implicit scope
+console.log("yes");
+else
+console.log("no");*/
+
+/*const score =200;
+if(score > 100)
+{
+let power = "fly";
+console.log(`User Power.: ${power}`);
+}
+// console.log(`User Power.: ${power}`);  // gives error 
+*/
+
+const bal =  400;  // don't prefer this prectice
+// if(bal > 500) console.log("Test1") ,console.log("Test2");
+
+// if - elseif - else statement
+
+// if(bal < 500)
+// {
+//     console.log("balance is less than 500");
+// }
+// if(bal < 750)
+// {
+//     console.log("balance is less than 750");
+// }
+// else 
+// {
+//     console.log(`Total balance is ${bal}`);
+// }
+
+
+const  userLoggedIn = true;
+const debitCard = true;
+
+const loggedInFromGoogle = false;
+const loggedInFromEmail = false;
+
+
+/*if(userLoggedIn && debitCard)
+{
+    console.log("User Allow to buy course.");
+}
+else
+console.log("please Login...");*/
+
+/*
+if(loggedInFromEmail || loggedInFromGoogle)
+console.log("User is Login.");
+else 
+console.log("Please try some another way for Login.");
+*/
+
+
