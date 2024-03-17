b245672019c1df5176985a9b1d5db16a88686269
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-03-17 05:36:30 +0000

    filter

---
+++ 05_itration/filter.js
@@ -0,0 +1,53 @@
+// const lang = ["js" , "java" , "python" , "c++" , "ruby"];
+
+// lang.forEach((item) => {;
+//     const value = item
+//      console.log(item);
+
+//      return value
+//     })
+
+//     console.log(value);
+
+
+
+//   filter
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNums =  myNums.filter((num) => num > 4)
+// console.log(newNums);
+
+
+// const nu =myNums.filter((n) =>{ return n >2 })
+// console.log(nu);
+
+
+const newNum = [];
+
+/*myNums.filter((num)=>{ 
+    if(num % 2 == 0 )
+    {
+        newNum.push(num)
+    }
+})
+console.log(newNum);
+*/
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+ const publish =  books.filter((book)=>{ return book.publish > 2000});
+//  console.log(publish);
+
+ const genre =  books.filter((book) => { return book.genre === "Non-Fiction"})
+ console.log(genre);
