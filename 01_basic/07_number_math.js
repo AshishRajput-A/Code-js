470cc904eb8c8151532a6c199986c7073b491a02
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-04-14 12:45:41 +0000

    date and number

--- 01_basic/07_number_math.js
+++ 01_basic/07_number_math.js
@@ -2,23 +2,31 @@ let score = 99;
 // console.log(score);
 // console.log(typeof score);
 
-const balance =  new Number(33);
+const balance =  new Number(334);
 // console.log(balance);
 
-// console.log("toString",balance.toString().length);
+// console.log("toString",balance.toString().length);  //3
 
 
-// console.log("toFixed.:",balance.toFixed(1));
+//  console.log("toFixed.:",balance.toFixed(2));  // 334.00
 
 const otherNum =  23.8966;
 
-// console.log("toPrecision.:",otherNum.toPrecision(4));
+// console.log("toPrecision.:",otherNum.toPrecision(5)); //23.897
 
 const zeros =  100000;
-// console.log(zeros.toLocaleString('en-IN'));
+//  console.log(zeros.toLocaleString('en-IN'));  //1,00,000
 
 const arr1 = [1,2,3,4,5]
 // console.log();
+
+
+const num1 =  new Number(88);
+
+console.log();
+
+
+
 
 
 
@@ -26,14 +34,13 @@ const arr1 = [1,2,3,4,5]
 
 
 // console.log(Math);
-// console.log("abs() .:",Math.abs(-4)); //conver negative to positive
-// console.log("round() .:",Math.round(4.53));
-// console.log("Ceil() .:", Math.ceil(4.2));
-// console.log("Floor() .:",Math.floor(45.455));
-// console.log("min() .:",Math.min(4,6,5,4,8,9));
+console.log("abs() .:",Math.abs(-4)); //conver negative to positive
+console.log("round() .:",Math.round(4.53));
+console.log("Ceil() .:", Math.ceil(4.2));
+console.log("Floor() .:",Math.floor(45.455));
+console.log("min() .:",Math.min(4,6,5,2,8,9));
 
-
-setInterval(()=>{
+/* setInterval(()=>{
 
     // console.log(Math.floor(Math.random() * 6+1) );
 
@@ -43,6 +50,15 @@ setInterval(()=>{
     console.log(Math.floor(Math.random() * (max - min +1 ) ) + min);
 
 
-}, 1000)
+}, 1000)*/
 
+
+setInterval(()=>{  
+
+    const min =10;
+    const max = 20;
+
+    console.log(Math.floor( Math.random() * (max - min + 1)) + min);
+
+} , 1000)
 
