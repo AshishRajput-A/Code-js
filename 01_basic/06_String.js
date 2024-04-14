c602fe74799e834ea686d43b08f118e089093faf
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-04-14 06:14:23 +0000

    string function

--- 01_basic/06_String.js
+++ 01_basic/06_String.js
@@ -17,9 +17,9 @@ const repoCount = 50;
 // console.log(name.substring(0,4)); // substring()
 // console.log(name.slice(-6,3)); //slice()
 
-const name1 =  "        Ashish      "
-// console.log(name1);
-// console.log(name1.trim());     // trim()
+const name1 =  "        Ashish     "
+console.log(name1);
+console.log(name1.trim());     // trim()
 
 const url ="https://ashish.com//ashish%20rajput";
 // console.log(url.replace("%20" , "-"));
@@ -28,4 +28,9 @@ const url ="https://ashish.com//ashish%20rajput";
 
 
 const game = "Ashish-Rajput-WedDev" // split()
-console.log(game.split("-")); // it split the screen acording condition
+// console.log(game.split("-")); // it split the line  according to condition condition
+
+const id = "Ashish is a nice boy"
+// console.log(id.split());     // [ 'Ashish' ]
+// console.log(id.split(''));   //[ 'A', 's', 'h', 'i', 's', 'h' ]
+// console.log(id.split(' '));  // [ 'Ashish', 'is', 'a', 'nice', 'boy' ]
