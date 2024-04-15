e31153c6f6e1428cc913339a0b1b86526d08feb9
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-04-15 04:22:18 +0000

    Array-1

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -12,15 +12,18 @@ const myarr = [0,1, 2, 3, 4, 5];
 console.log( "myarr .:" ,myarr);
 
 // myarr.push(6, 7, 8, 9, 10);
+// console.log(myarr);                 // add data end of the array
+
+
+// myarr.pop();                        // remove data end of the array  
 // console.log(myarr);
 
-// myarr.pop();
+
+// myarr.unshift(9);                   // add data start of the array
 // console.log(myarr);
 
-// myarr.unshift(9);
-// console.log(myarr);
 
-// myarr.shift();
+// myarr.shift();                     // remove data start of the array
 // console.log(myarr);   
 
 // console.log(myarr.includes(6));  // false
@@ -36,12 +39,12 @@ const newarr = myarr.join(""); // join() convert array to string
 // slice spice  IMP Question
 
  const myslice =  myarr.slice(1,3)
- console.log("myslice.:" , myslice);  // slice just cut the value
+ console.log("myslice.:" , myslice);  // slice just  take porstion value and don't disturb array
  console.log( "myarr .:" ,myarr);
 
 
 const mysplice =  myarr.splice(1,3);
-console.log("mysplice",mysplice);   // splice cut the value from array
+console.log("mysplice",mysplice);   // splice take porstion value from refrence  array
 console.log( "myarr .:" ,myarr);
 
 
