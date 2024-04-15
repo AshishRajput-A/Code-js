a7e6bc13acc62b4598720203d542c599c2dbfb1b
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-04-15 04:17:01 +0000

    date

--- 01_basic/08_date.js
+++ 01_basic/08_date.js
@@ -5,30 +5,45 @@ const date =  new Date();
 // console.log(  date);
 
 
-// console.log("toString() .: ",date.toString());
+// console.log("toString() .: ",date.toString());  // Sun Apr 14 2024 12:53:31 GMT+0000 (Coordinated Universal Time)
 
-// console.log("toLocaleTimeString() .:",date.toLocaleTimeString());
+// console.log("toLocaleTimeString() .:",date.toLocaleTimeString());  // 12:53:31 PM
 
-// console.log("toLocaleDateString().:",date.toLocaleDateString());
+// console.log("toLocaleDateString().:",date.toLocaleDateString());  // 4/14/2024
 
-// console.log("toJSON() .:",date.toJSON());
+// console.log("toJSON() .:",date.toJSON());  //2024-04-14T12:53:31.928Z
 
-// console.log("toLocaleString().:",date.toLocaleString());
+// console.log("toLocaleString().:",date.toLocaleString()); // 4/14/2024, 12:53:31 PM
 
 
-let myDate1 = new Date(2024, 1 , 24);
+let myDate1 = new Date(2024, 3 , 14);
 // console.log("toDateString() .:",myDate1.toDateString());
 
-let myDate2 =  new Date("02-25-2024");
+let myDate2 =  new Date(2024, 3 ,14 , 18,31);
 // console.log(myDate2.toLocaleString());
 
-let timeStemp =  Date.now();
-// console.log(timeStemp);
-// console.log(myDate2.getTime());
+// MM//DD/YYYY
+const date1 =  new Date("01-14-2014")
+// console.log(date1.toLocaleString());
 
-let newDate = new Date("01-26-2024");
+// yyyy/mm/dd
+const date2 =  new Date("2023-04-14")
+// console.log(date2.toLocaleString());
+
+
+
+
+
+
+let newDate = new Date();
 console.log(newDate);
 console.log(newDate.getMonth());
 console.log(newDate.getDay());
 
-newDate.toLocaleString('default' ,{  weekday : "long" , timeZone : ""})
+console.log(newDate.getTime());
+
+
+
+
+
+
