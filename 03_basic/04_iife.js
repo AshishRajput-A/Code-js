68f8ac4256a9adbbef28382c68acfedc9aa3b5a1
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-04-18 04:37:50 +0000

    iife

--- 03_basic/04_iife.js
+++ 03_basic/04_iife.js
@@ -23,4 +23,25 @@
 
 
 
-//  kai baar global scope ke poluction  se  bachne ke liye iife ka use krte he 
+//  kai baar global scope ke poluction  se  bachne ke liye iife ka use krte he function 
+
+function  one() 
+{
+    
+    two()
+    console.log("one");
+}
+
+function  two() 
+
+{
+    three()
+    console.log("two");
+}
+
+function  three() 
+{
+    console.log("three");
+}
+
+one()
