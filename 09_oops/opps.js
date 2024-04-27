36b02b0dbd568df02cf1f997a611c960d41be167
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-04-27 16:43:52 +0000

    object litral and  this

---
+++ 09_oops/opps.js
@@ -0,0 +1,27 @@
+/*const user = {
+    name : "Ashish", 
+    age : 20,
+    city : "Ahmedabad",
+
+    getAllDetail : function() {
+        console.log(this);
+    }
+}
+
+console.log(user.name);
+console.log(user.getAllDetail());
+*/
+
+
+function user (name , age) 
+{
+    this.name = name;             // this keyword makes new object {} that means this.name is new object
+    this.age = age;
+}
+
+const  user1 = new user("Ashish" , 20);
+const  user2 = new user("Vijay" , 16)
+
+
+  console.log(user1);
+  console.log(user2);
