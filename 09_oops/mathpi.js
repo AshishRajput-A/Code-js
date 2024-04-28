e40fa6e45bc0e7dd4ef47c01c04cb822f6bdc1a8
RAJPUT ASHISH <151988908+AshishRajput-A@users.noreply.github.com>
2024-04-28 16:46:15 +0000

    all new think about object

---
+++ 09_oops/mathpi.js
@@ -0,0 +1,40 @@
+
+const discripter = Object.getOwnPropertyDescriptor(Math ,  "PI");
+
+// console.log(discripter);
+
+
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+
+const chai = {
+    name : "ginger chai",
+    price : 250,
+    isAvalible: true,
+    order : function() {
+        console.log('chai nhi bani');
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+Object.defineProperty(chai,'name' , {
+    // writable: false,
+    enumerable:false,
+    // configurable:false
+})
+
+// console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+
+for (const [key , value] of Object.entries(chai)) {
+
+    if(typeof value !== 'function')
+    {
+        console.log(`${key} : ${value}`);
+    }
+    
+   
+}
